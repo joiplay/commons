db63a0e268ee8cf5dbb6a09eafb97beff43ae498
joiplay <joiplay@protonmail.com>
2021-10-20 11:08:03 +0300

    Add loadFromFile and saveToFile methods to GamePadParser

--- src/main/java/cyou/joiplay/commons/file/FileUtils.java
+++ src/main/java/cyou/joiplay/commons/file/FileUtils.java
@@ -2,12 +2,18 @@ package cyou.joiplay.commons.file;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
 
 public class FileUtils {
     static public void copy(String sourcePath, String targetPath) throws IOException {
@@ -25,5 +31,24 @@ public class FileUtils {
             out.write(buffer, 0, lengthRead);
             out.flush();
         }
+    }
+
+    static public String readText(File file) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+
+        while((line = bufferedReader.readLine()) != null){
+            stringBuilder.append(line);
+            stringBuilder.append(System.lineSeparator());
+        }
+
+        return stringBuilder.toString();
+    }
+
+    static public void writeText(File file, String string) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+        bufferedWriter.write(string);
+        bufferedWriter.close();
     }
 }
--- src/main/java/cyou/joiplay/commons/parser/GamePadParser.java
+++ src/main/java/cyou/joiplay/commons/parser/GamePadParser.java
@@ -1,11 +1,18 @@
 package cyou.joiplay.commons.parser;
 
+import android.util.Log;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.IOException;
+
+import cyou.joiplay.commons.file.FileUtils;
 import cyou.joiplay.commons.model.GamePad;
 
 public class GamePadParser {
+    private static final String TAG = "GamePadParser";
     public static void parse(GamePad gamePad, String jsonString) throws JSONException{
         JSONObject jsonObject = new JSONObject(jsonString);
 
@@ -50,5 +57,65 @@ public class GamePadParser {
         }
 
         return;
+    }
+
+    public static void loadFromFile(GamePad gamePad, File file) throws IOException, JSONException {
+        JSONObject jsonObject = new JSONObject(FileUtils.readText(file));
+
+        if (jsonObject.has("btnOpacity")){
+            gamePad.btnOpacity = jsonObject.getInt("btnOpacity");
+        }
+
+        if (jsonObject.has("btnScale")){
+            gamePad.btnScale = jsonObject.getInt("btnScale");
+        }
+
+        if (jsonObject.has("aKeyCode")){
+            gamePad.aKeyCode = jsonObject.getInt("aKeyCode");
+        }
+
+        if (jsonObject.has("bKeyCode")){
+            gamePad.bKeyCode = jsonObject.getInt("bKeyCode");
+        }
+
+        if (jsonObject.has("cKeyCode")){
+            gamePad.cKeyCode = jsonObject.getInt("cKeyCode");
+        }
+
+        if (jsonObject.has("xKeyCode")){
+            gamePad.xKeyCode = jsonObject.getInt("xKeyCode");
+        }
+
+        if (jsonObject.has("yKeyCode")){
+            gamePad.yKeyCode = jsonObject.getInt("yKeyCode");
+        }
+
+        if (jsonObject.has("zKeyCode")){
+            gamePad.zKeyCode = jsonObject.getInt("zKeyCode");
+        }
+
+        if (jsonObject.has("lKeyCode")){
+            gamePad.lKeyCode = jsonObject.getInt("lKeyCode");
+        }
+
+        if (jsonObject.has("rKeyCode")){
+            gamePad.rKeyCode = jsonObject.getInt("rKeyCode");
+        }
+    }
+
+    public static void saveToFile(GamePad gamePad, File file) throws JSONException, IOException {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("btnOpacity", gamePad.btnOpacity);
+        jsonObject.put("btnScale", gamePad.btnScale);
+        jsonObject.put("aKeyCode", gamePad.aKeyCode);
+        jsonObject.put("bKeyCode", gamePad.bKeyCode);
+        jsonObject.put("cKeyCode", gamePad.cKeyCode);
+        jsonObject.put("xKeyCode", gamePad.xKeyCode);
+        jsonObject.put("yKeyCode", gamePad.yKeyCode);
+        jsonObject.put("zKeyCode", gamePad.zKeyCode);
+        jsonObject.put("lKeyCode", gamePad.lKeyCode);
+        jsonObject.put("rKeyCode", gamePad.rKeyCode);
+
+        FileUtils.writeText(file, jsonObject.toString(4));
     }
 }
