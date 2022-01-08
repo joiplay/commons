6ee71e7691deafd1d8022baea2e209319e73ac1f
joiplay <joiplay@protonmail.com>
2022-01-08 12:08:21 +0300

    Add default key codes

--- src/main/java/cyou/joiplay/commons/model/GamePad.java
+++ src/main/java/cyou/joiplay/commons/model/GamePad.java
@@ -1,15 +1,17 @@
 package cyou.joiplay.commons.model;
 
+import android.view.KeyEvent;
+
 public class GamePad {
     public Boolean diagonalMovement = false;
     public Integer btnOpacity = 100;
     public Integer btnScale = 100;
-    public Integer aKeyCode = 0;
-    public Integer bKeyCode = 0;
-    public Integer cKeyCode = 0;
-    public Integer xKeyCode = 0;
-    public Integer yKeyCode = 0;
-    public Integer zKeyCode = 0;
-    public Integer lKeyCode = 0;
-    public Integer rKeyCode = 0;
+    public Integer aKeyCode = KeyEvent.KEYCODE_X;
+    public Integer bKeyCode = KeyEvent.KEYCODE_SHIFT_LEFT;
+    public Integer cKeyCode = KeyEvent.KEYCODE_R;
+    public Integer xKeyCode = KeyEvent.KEYCODE_Z;
+    public Integer yKeyCode = KeyEvent.KEYCODE_CTRL_LEFT;
+    public Integer zKeyCode = KeyEvent.KEYCODE_Q;
+    public Integer lKeyCode = KeyEvent.KEYCODE_ENTER;
+    public Integer rKeyCode = KeyEvent.KEYCODE_ESCAPE;
 }
