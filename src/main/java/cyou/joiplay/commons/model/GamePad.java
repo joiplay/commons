216e0fc230a28415df2d3bf7abef31c4477eca27
joiplay <joiplay@protonmail.com>
2023-01-05 12:36:29 +0300

    Update model and parser for GamePad Adds alternative key set and hideGamepad variable

--- src/main/java/cyou/joiplay/commons/model/GamePad.java
+++ src/main/java/cyou/joiplay/commons/model/GamePad.java
@@ -4,16 +4,17 @@ import android.view.KeyEvent;
 
 public class GamePad {
     public Boolean diagonalMovement = false;
+    public Boolean hideGamepad = false;
     public Integer btnOpacity = 100;
     public Integer btnScale = 100;
-    public Integer aKeyCode = KeyEvent.KEYCODE_X;
-    public Integer bKeyCode = KeyEvent.KEYCODE_SHIFT_LEFT;
-    public Integer cKeyCode = KeyEvent.KEYCODE_R;
-    public Integer xKeyCode = KeyEvent.KEYCODE_Z;
-    public Integer yKeyCode = KeyEvent.KEYCODE_CTRL_LEFT;
-    public Integer zKeyCode = KeyEvent.KEYCODE_Q;
-    public Integer lKeyCode = KeyEvent.KEYCODE_ENTER;
-    public Integer rKeyCode = KeyEvent.KEYCODE_ESCAPE;
-    public Integer clKeyCode = KeyEvent.KEYCODE_F2;
-    public Integer crKeyCode = KeyEvent.KEYCODE_F8;
+    public Integer[] aKeyCode = new Integer[]{KeyEvent.KEYCODE_X, KeyEvent.KEYCODE_W};
+    public Integer[] bKeyCode = new Integer[]{KeyEvent.KEYCODE_SHIFT_LEFT, KeyEvent.KEYCODE_A};
+    public Integer[] cKeyCode = new Integer[]{KeyEvent.KEYCODE_R, KeyEvent.KEYCODE_S};
+    public Integer[] xKeyCode = new Integer[]{KeyEvent.KEYCODE_Z, KeyEvent.KEYCODE_D};
+    public Integer[] yKeyCode = new Integer[]{KeyEvent.KEYCODE_CTRL_LEFT, KeyEvent.KEYCODE_C};
+    public Integer[] zKeyCode = new Integer[]{KeyEvent.KEYCODE_Q, KeyEvent.KEYCODE_V};
+    public Integer[] lKeyCode = new Integer[]{KeyEvent.KEYCODE_ENTER,KeyEvent.KEYCODE_ENTER};
+    public Integer[] rKeyCode = new Integer[]{KeyEvent.KEYCODE_ESCAPE, KeyEvent.KEYCODE_ESCAPE};
+    public Integer[] clKeyCode = new Integer[]{KeyEvent.KEYCODE_F2, KeyEvent.KEYCODE_F2};
+    public Integer[] crKeyCode = new Integer[]{KeyEvent.KEYCODE_F8, KeyEvent.KEYCODE_F8};
 }
--- src/main/java/cyou/joiplay/commons/parser/GamePadParser.java
+++ src/main/java/cyou/joiplay/commons/parser/GamePadParser.java
@@ -20,6 +20,10 @@ public class GamePadParser {
                 gamePad.diagonalMovement = gamepadObject.getJSONObject("diagonalMovement").getBoolean("boolean");
             }
 
+            if (gamepadObject.has("hideGamepad")){
+                gamePad.hideGamepad = gamepadObject.getJSONObject("hideGamepad").getBoolean("boolean");
+            }
+
             if (gamepadObject.has("btnOpacity")){
                 gamePad.btnOpacity = gamepadObject.getJSONObject("btnOpacity").getInt("int");
             }
@@ -29,43 +33,83 @@ public class GamePadParser {
             }
 
             if (gamepadObject.has("aKeyCode")){
-                gamePad.aKeyCode = gamepadObject.getJSONObject("aKeyCode").getInt("int");
+                gamePad.aKeyCode[0] = gamepadObject.getJSONObject("aKeyCode").getInt("int");
+            }
+
+            if (gamepadObject.has("aKeyCode1")){
+                gamePad.aKeyCode[1] = gamepadObject.getJSONObject("aKeyCode1").getInt("int");
             }
 
             if (gamepadObject.has("bKeyCode")){
-                gamePad.bKeyCode = gamepadObject.getJSONObject("bKeyCode").getInt("int");
+                gamePad.bKeyCode[0] = gamepadObject.getJSONObject("bKeyCode").getInt("int");
+            }
+
+            if (gamepadObject.has("bKeyCode1")){
+                gamePad.bKeyCode[1] = gamepadObject.getJSONObject("bKeyCode1").getInt("int");
             }
 
             if (gamepadObject.has("cKeyCode")){
-                gamePad.cKeyCode = gamepadObject.getJSONObject("cKeyCode").getInt("int");
+                gamePad.cKeyCode[0] = gamepadObject.getJSONObject("cKeyCode").getInt("int");
+            }
+
+            if (gamepadObject.has("cKeyCode1")){
+                gamePad.cKeyCode[1] = gamepadObject.getJSONObject("cKeyCode1").getInt("int");
             }
 
             if (gamepadObject.has("xKeyCode")){
-                gamePad.xKeyCode = gamepadObject.getJSONObject("xKeyCode").getInt("int");
+                gamePad.xKeyCode[0] = gamepadObject.getJSONObject("xKeyCode").getInt("int");
+            }
+
+            if (gamepadObject.has("xKeyCode1")){
+                gamePad.xKeyCode[1] = gamepadObject.getJSONObject("xKeyCode1").getInt("int");
             }
 
             if (gamepadObject.has("yKeyCode")){
-                gamePad.yKeyCode = gamepadObject.getJSONObject("yKeyCode").getInt("int");
+                gamePad.yKeyCode[0] = gamepadObject.getJSONObject("yKeyCode").getInt("int");
+            }
+
+            if (gamepadObject.has("yKeyCode1")){
+                gamePad.yKeyCode[1] = gamepadObject.getJSONObject("yKeyCode1").getInt("int");
             }
 
             if (gamepadObject.has("zKeyCode")){
-                gamePad.zKeyCode = gamepadObject.getJSONObject("zKeyCode").getInt("int");
+                gamePad.zKeyCode[0] = gamepadObject.getJSONObject("zKeyCode").getInt("int");
+            }
+
+            if (gamepadObject.has("zKeyCode1")){
+                gamePad.zKeyCode[1] = gamepadObject.getJSONObject("zKeyCode1").getInt("int");
             }
 
             if (gamepadObject.has("lKeyCode")){
-                gamePad.lKeyCode = gamepadObject.getJSONObject("lKeyCode").getInt("int");
+                gamePad.lKeyCode[0] = gamepadObject.getJSONObject("lKeyCode").getInt("int");
+            }
+
+            if (gamepadObject.has("lKeyCode1")){
+                gamePad.lKeyCode[1] = gamepadObject.getJSONObject("lKeyCode1").getInt("int");
             }
 
             if (gamepadObject.has("rKeyCode")){
-                gamePad.rKeyCode = gamepadObject.getJSONObject("rKeyCode").getInt("int");
+                gamePad.rKeyCode[0] = gamepadObject.getJSONObject("rKeyCode").getInt("int");
+            }
+
+            if (gamepadObject.has("rKeyCode1")){
+                gamePad.rKeyCode[1] = gamepadObject.getJSONObject("rKeyCode1").getInt("int");
             }
 
             if (gamepadObject.has("clKeyCode")){
-                gamePad.clKeyCode = gamepadObject.getJSONObject("clKeyCode").getInt("int");
+                gamePad.clKeyCode[0] = gamepadObject.getJSONObject("clKeyCode").getInt("int");
+            }
+
+            if (gamepadObject.has("clKeyCode1")){
+                gamePad.clKeyCode[1] = gamepadObject.getJSONObject("clKeyCode1").getInt("int");
             }
 
             if (gamepadObject.has("crKeyCode")){
-                gamePad.crKeyCode = gamepadObject.getJSONObject("crKeyCode").getInt("int");
+                gamePad.crKeyCode[0] = gamepadObject.getJSONObject("crKeyCode").getInt("int");
+            }
+
+            if (gamepadObject.has("crKeyCode1")){
+                gamePad.crKeyCode[1] = gamepadObject.getJSONObject("crKeyCode1").getInt("int");
             }
         }
     }
@@ -77,6 +121,10 @@ public class GamePadParser {
             gamePad.diagonalMovement = jsonObject.getBoolean("diagonalMovement");
         }
 
+        if (jsonObject.has("hideGamepad")){
+            gamePad.hideGamepad = jsonObject.getBoolean("hideGamepad");
+        }
+
         if (jsonObject.has("btnOpacity")){
             gamePad.btnOpacity = jsonObject.getInt("btnOpacity");
         }
@@ -86,61 +134,112 @@ public class GamePadParser {
         }
 
         if (jsonObject.has("aKeyCode")){
-            gamePad.aKeyCode = jsonObject.getInt("aKeyCode");
+            gamePad.aKeyCode[0] = jsonObject.getInt("aKeyCode");
+        }
+
+        if (jsonObject.has("aKeyCode1")){
+            gamePad.aKeyCode[1] = jsonObject.getInt("aKeyCode1");
         }
 
         if (jsonObject.has("bKeyCode")){
-            gamePad.bKeyCode = jsonObject.getInt("bKeyCode");
+            gamePad.bKeyCode[0] = jsonObject.getInt("bKeyCode");
+        }
+
+        if (jsonObject.has("bKeyCode1")){
+            gamePad.bKeyCode[1] = jsonObject.getInt("bKeyCode1");
         }
 
         if (jsonObject.has("cKeyCode")){
-            gamePad.cKeyCode = jsonObject.getInt("cKeyCode");
+            gamePad.cKeyCode[0] = jsonObject.getInt("cKeyCode");
+        }
+
+        if (jsonObject.has("cKeyCode1")){
+            gamePad.cKeyCode[1] = jsonObject.getInt("cKeyCode1");
         }
 
         if (jsonObject.has("xKeyCode")){
-            gamePad.xKeyCode = jsonObject.getInt("xKeyCode");
+            gamePad.xKeyCode[0] = jsonObject.getInt("xKeyCode");
+        }
+
+        if (jsonObject.has("xKeyCode1")){
+            gamePad.xKeyCode[1] = jsonObject.getInt("xKeyCode1");
         }
 
         if (jsonObject.has("yKeyCode")){
-            gamePad.yKeyCode = jsonObject.getInt("yKeyCode");
+            gamePad.yKeyCode[0] = jsonObject.getInt("yKeyCode");
+        }
+
+        if (jsonObject.has("yKeyCode1")){
+            gamePad.yKeyCode[1] = jsonObject.getInt("yKeyCode1");
         }
 
         if (jsonObject.has("zKeyCode")){
-            gamePad.zKeyCode = jsonObject.getInt("zKeyCode");
+            gamePad.zKeyCode[0] = jsonObject.getInt("zKeyCode");
+        }
+
+        if (jsonObject.has("zKeyCode1")){
+            gamePad.zKeyCode[1] = jsonObject.getInt("zKeyCode1");
         }
 
         if (jsonObject.has("lKeyCode")){
-            gamePad.lKeyCode = jsonObject.getInt("lKeyCode");
+            gamePad.lKeyCode[0] = jsonObject.getInt("lKeyCode");
+        }
+
+        if (jsonObject.has("lKeyCode1")){
+            gamePad.lKeyCode[1] = jsonObject.getInt("lKeyCode1");
         }
 
         if (jsonObject.has("rKeyCode")){
-            gamePad.rKeyCode = jsonObject.getInt("rKeyCode");
+            gamePad.rKeyCode[0] = jsonObject.getInt("rKeyCode");
+        }
+
+        if (jsonObject.has("rKeyCode1")){
+            gamePad.rKeyCode[1] = jsonObject.getInt("rKeyCode1");
         }
 
         if (jsonObject.has("clKeyCode")){
-            gamePad.clKeyCode = jsonObject.getInt("clKeyCode");
+            gamePad.clKeyCode[0] = jsonObject.getInt("clKeyCode");
+        }
+
+        if (jsonObject.has("clKeyCode1")){
+            gamePad.clKeyCode[1] = jsonObject.getInt("clKeyCode1");
         }
 
         if (jsonObject.has("crKeyCode")){
-            gamePad.crKeyCode = jsonObject.getInt("crKeyCode");
+            gamePad.crKeyCode[0] = jsonObject.getInt("crKeyCode");
+        }
+
+        if (jsonObject.has("crKeyCode1")){
+            gamePad.crKeyCode[1] = jsonObject.getInt("crKeyCode1");
         }
     }
 
     public static void saveToFile(GamePad gamePad, File file) throws JSONException, IOException {
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("diagonalMovement", gamePad.diagonalMovement);
+        jsonObject.put("hideGamepad", gamePad.hideGamepad);
         jsonObject.put("btnOpacity", gamePad.btnOpacity);
         jsonObject.put("btnScale", gamePad.btnScale);
-        jsonObject.put("aKeyCode", gamePad.aKeyCode);
-        jsonObject.put("bKeyCode", gamePad.bKeyCode);
-        jsonObject.put("cKeyCode", gamePad.cKeyCode);
-        jsonObject.put("xKeyCode", gamePad.xKeyCode);
-        jsonObject.put("yKeyCode", gamePad.yKeyCode);
-        jsonObject.put("zKeyCode", gamePad.zKeyCode);
-        jsonObject.put("lKeyCode", gamePad.lKeyCode);
-        jsonObject.put("rKeyCode", gamePad.rKeyCode);
-        jsonObject.put("clKeyCode", gamePad.clKeyCode);
-        jsonObject.put("crKeyCode", gamePad.crKeyCode);
+        jsonObject.put("aKeyCode", gamePad.aKeyCode[0]);
+        jsonObject.put("bKeyCode", gamePad.bKeyCode[0]);
+        jsonObject.put("cKeyCode", gamePad.cKeyCode[0]);
+        jsonObject.put("xKeyCode", gamePad.xKeyCode[0]);
+        jsonObject.put("yKeyCode", gamePad.yKeyCode[0]);
+        jsonObject.put("zKeyCode", gamePad.zKeyCode[0]);
+        jsonObject.put("lKeyCode", gamePad.lKeyCode[0]);
+        jsonObject.put("rKeyCode", gamePad.rKeyCode[0]);
+        jsonObject.put("clKeyCode", gamePad.clKeyCode[0]);
+        jsonObject.put("crKeyCode", gamePad.crKeyCode[0]);
+        jsonObject.put("aKeyCode1", gamePad.aKeyCode[1]);
+        jsonObject.put("bKeyCode1", gamePad.bKeyCode[1]);
+        jsonObject.put("cKeyCode1", gamePad.cKeyCode[1]);
+        jsonObject.put("xKeyCode1", gamePad.xKeyCode[1]);
+        jsonObject.put("yKeyCode1", gamePad.yKeyCode[1]);
+        jsonObject.put("zKeyCode1", gamePad.zKeyCode[1]);
+        jsonObject.put("lKeyCode1", gamePad.lKeyCode[1]);
+        jsonObject.put("rKeyCode1", gamePad.rKeyCode[1]);
+        jsonObject.put("clKeyCode1", gamePad.clKeyCode[1]);
+        jsonObject.put("crKeyCode1", gamePad.crKeyCode[1]);
 
         FileUtils.writeText(file, jsonObject.toString(4));
     }
